519688854cd58da97b71630d39094a9809f860a0
James Raygada <james@LAPTOP-TJDUGKD9.localdomain>
2025-10-16 13:09:40 +0100

    Add PoC Extraction Laravel skeleton (Step 3)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{ContractController, ResultController};
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ContractController::class,'index'])->name('contracts.index');
+Route::get('/contracts/{publicId}', [ContractController::class,'show'])->name('contracts.show');
+Route::post('/contracts', [ContractController::class,'store'])->name('contracts.store');
+Route::post('/contracts/{publicId}/rerun', [ContractController::class,'rerun'])->name('contracts.rerun');
+Route::post('/results/{id}/override', [ResultController::class,'override'])->name('results.override');
